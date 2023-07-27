62d38241fb222a5a1509612071a6b2c5c386d822
thanhluan-36 <vuvuthanhluan@gmail.com>
2023-07-27 23:59:10 +0700

    init commit social-admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/sb-admin-2.min.css";
+import { Login } from "./pages/Account/Login";
+import { Admin } from "./pages/Admin/Admin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Admin />
+        </div>
+    );
 }
 
 export default App;
